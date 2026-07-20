562eb98bd0df04cfab1e794431ed68aae7f59669
Douyin-lao6pc <ljh18259239225@outlook.com>
2026-07-20 16:34:55 +0800

    Update users.js

--- users.js
+++ users.js
@@ -2,4 +2,5 @@
 const users = [
     { username: 'lao6pc', pwdHash: '27a1db5d6b63f23477d8cb769554531a82340f8ed7ed1f1c534d6e17057300c9' },
     { username: 'xiyun', pwdHash: 'ed8b0c116e03302f99beefec92f965c3f1a5484272ab795a471460d3b91a51cd' },
+    { username: '豆豆', pwdHash: '17ca7091a72b8a7aeeafdeb3cb18cf8f78c3ea7becf370db61dfd4ab52b2ea0d' },
 ];
