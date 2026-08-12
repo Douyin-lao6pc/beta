dbed8dce123d30bfa09fc60ead42db57104f2629
Douyin-lao6pc <ljh18259239225@outlook.com>
2026-08-12 17:23:30 +0800

    Add files via upload

--- users.js
+++ users.js
@@ -1,8 +1,8 @@
 
 const users = [
-    { username: 'lao6pc', pwdHash: '27a1db5d6b63f23477d8cb769554531a82340f8ed7ed1f1c534d6e17057300c9' },
-    { username: 'xiyun', pwdHash: 'ed8b0c116e03302f99beefec92f965c3f1a5484272ab795a471460d3b91a51cd' },
-    { username: '豆豆', pwdHash: '17ca7091a72b8a7aeeafdeb3cb18cf8f78c3ea7becf370db61dfd4ab52b2ea0d' },
-    { username: 'bshb1234', pwdHash: 'b69810027317e1f46462f2db02927893ff6992efccc7332fb1003a0dfdc7e46f' },
-    { username: '114514', pwdHash: 'a4c4dc6e2e716abe471313ec701e347ea5dc6bc66c285311424f81767a4d983f' },
+    { username: 'lao6pc', pwdHash: '27a1db5d6b63f23477d8cb769554531a82340f8ed7ed1f1c534d6e17057300c9',email:"*" },
+    { username: 'xiyun', pwdHash: 'ed8b0c116e03302f99beefec92f965c3f1a5484272ab795a471460d3b91a51cd',email:"#" },
+    { username: '豆豆', pwdHash: '17ca7091a72b8a7aeeafdeb3cb18cf8f78c3ea7becf370db61dfd4ab52b2ea0d',email:"#" },
+    { username: 'bshb1234', pwdHash: 'b69810027317e1f46462f2db02927893ff6992efccc7332fb1003a0dfdc7e46f',email:"#" },
+    { username: '114514', pwdHash: 'a4c4dc6e2e716abe471313ec701e347ea5dc6bc66c285311424f81767a4d983f',email:"#" },
 ];
